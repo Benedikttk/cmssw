55ec4af072e6d2336b983f3cfd7deff91d9db4af
Benedikt Tor Kristinsson <bkristinsson@benediktkristinssonsummerstudent.dyndns.cern.ch>
2024-07-18 15:34:10 +0200

    Adding first formating trials

--- Validation/TrackerDigis/python/stripDigisValidation_cfi.py
+++ Validation/TrackerDigis/python/stripDigisValidation_cfi.py
@@ -1,12 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
 from DQMServices.Core.DQMEDAnalyzer import DQMEDAnalyzer
-stripDigisValid = DQMEDAnalyzer('SiStripDigiValid',
-    src = cms.InputTag("simSiStripDigis","ZeroSuppressed"),
-    runStandalone = cms.bool(False),
-    outputFile = cms.untracked.string(''),
-    verbose = cms.untracked.bool(False)
+
+stripDigisValid = DQMEDAnalyzer(
+    "SiStripDigiValid",
+    src=cms.InputTag("simSiStripDigis", "ZeroSuppressed"),
+    runStandalone=cms.bool(False),
+    outputFile=cms.untracked.string(""),
+    verbose=cms.untracked.bool(False),
 )
-
-
-
